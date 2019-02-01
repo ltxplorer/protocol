f61d8c656410a1d9e51ea48b1958e24b7dcc4483
nfleury <nicholasfleury@gmail.com>
2019-02-01 10:26:44 +0400

    Add transfer model

---
+++ src/main/java/io/trxplorer/protocol/model/TransferModel.java
@@ -0,0 +1,77 @@
+package io.trxplorer.protocol.model;
+
+public class TransferModel {
+
+	private String hash;
+	
+	private long amount;
+	
+	private String token;
+	
+	private String from;
+	
+	private String to;
+	
+	private int type;
+	
+	private long timestamp;
+	
+	public String getHash() {
+		return hash;
+	}
+	
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+	
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	
+	
+}
